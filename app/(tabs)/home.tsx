686d5521f1ddf8083dd60feb6387d5a967fe8651
Katzuuu <dodookormos@gmail.com>
2024-07-26 16:26:10 +0200

    home header

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import Header from "@/components/home/header";
 
 const home = () => {
   return (
     <View>
-      <Text>home</Text>
+      <Header />
     </View>
   );
 };
